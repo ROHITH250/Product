667b4395cac74447300dc4a981e2af5e48d09689
G.Rohith <76099851+ROHITH250@users.noreply.github.com>
2024-04-16 10:13:41 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,23 @@
+const COLOR_BTNS = document.querySelectorAll('.color');
+COLOR_BTNS.forEach(color => {
+    color.addEventListener('click', () => {
+        let colorNameClass = color.className;
+        if(!color.classList.contains('active-color')){
+            let colorName = colorNameClass.slice(colorNameClass.indexOf('-') + 1, colorNameClass.length);
+            resetActiveBtns();
+            color.classList.add('active-color');
+            setNewColor(colorName)
+        }
+    });
+})
+
+function resetActiveBtns(){
+    COLOR_BTNS.forEach(color => {
+        color.classList.remove('active-color');
+    });
+}
+
+
+function setNewColor(color){
+    document.querySelector('.banner-right img').src = `images/tshirt_${color}.png`;
+}
